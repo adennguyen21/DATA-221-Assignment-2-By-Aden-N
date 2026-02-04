38df7a55fa84e74d62d3848e054670dc7fab48c0
adennguyen21 <adennguyen21@gmail.com>
2026-02-04 00:22:23 -0700

    Finish Question 1 for Assignment 2.

--- Question 1.py
+++ Question 1.py
@@ -1,4 +1,84 @@
 # Question 1 - Reading and processing a text file.
+# =============================================================
 
-file_handler = open("sample-file.txt", "r")
+def open_and_read_file(file_name): # Opens and reads the file, returning the contents as a list.
+    file_handler = open(file_name, "r")
+    return file_handler.readlines()
 
+
+def split_file_into_tokens(file_list): # Splits the contents of the file into individual words, which are then put into another list.
+    word_list = []
+    unfinished_word = " "
+
+    for line in file_list:
+        for letter in line:
+            if letter in [" ", "\n", ".", ",", "!", "?"]: # Stops connecting letters when it detects whitespace, "/n", or punctuation. (This forms the individual words)
+                word_list.append(unfinished_word) # Appends the word to word_list.
+                unfinished_word = " "
+            else:
+                unfinished_word = unfinished_word + letter # Connects letters to form words, until the if-condition is true.
+
+    return word_list
+
+
+def convert_and_clean_tokens(unfinished_word_list): # Converts all the words into lowercase, removes whitespaces, and filters out words that contain less than 2 letters.
+    finished_word_list = []
+
+    for i in range(len(unfinished_word_list)): # Removes whitespaces next to words, and converts them to lowercase.
+        unfinished_word_list[i] = unfinished_word_list[i].strip().lower()
+
+    while "" in unfinished_word_list: # Removes any empty elements from the list.
+        unfinished_word_list.remove("")
+
+    for word in unfinished_word_list: # Filters out any words that are less than 2 letters in length.
+        if len(word) >= 2:
+          finished_word_list.append(word) # Appends the cleaned up word into a list.
+
+    return finished_word_list
+
+
+def calculate_word_frequency(word_list): # Calculates the frequency of each word.
+    word_frequencies = {}
+
+    for word in word_list: # Traverses through every word in the cleaned up word list.
+        word_count = 0
+
+        for comparing_word in word_list: # Compares the word from the outer loop to this inner loop word.
+            if word == comparing_word:
+                word_count += 1 # Count goes up for every word that matches with the word from the outer loop.
+
+        word_frequencies[word] = word_count # Adds the word as a key, and the frequency of that word as the value.
+
+    sorted_word_frequencies = dict(sorted(word_frequencies.items(), key=lambda item: item[1], reverse=True))
+    # Sorts the dictionary by value in descending order.
+    return sorted_word_frequencies
+
+
+def print_top_10_most_frequent_words(word_frequencies): # Prints out the top ten most frequent words.
+    top_10_count = 0 # Once this count reaches 10, this function stops printing.
+
+    for item in word_frequencies.items(): # Goes through each element in the frequencies' library.
+        if top_10_count < 10:
+            word, frequency = item # Separates the word (key) and frequency number (value) as two different variables.
+            print(f"{word} -> {frequency}") # Prints out the result.
+            top_10_count += 1
+
+        else:
+            break
+
+# =================================================================================
+
+def main(): # Main function to call the other functions in.
+    text_file = "sample-file.txt"
+
+    unfinished_file_list = open_and_read_file(text_file)
+
+    unfinished_word_list = split_file_into_tokens(unfinished_file_list)
+
+    finished_word_list = convert_and_clean_tokens(unfinished_word_list)
+
+    word_frequencies = calculate_word_frequency(finished_word_list)
+
+    print_top_10_most_frequent_words(word_frequencies)
+
+main()
