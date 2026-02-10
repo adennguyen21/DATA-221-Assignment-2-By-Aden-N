46daf46404286067f41e0de6a7891e845c17be32
adennguyen21 <adennguyen21@gmail.com>
2026-02-10 12:49:26 -0700

    Finish rough code for Question 6, Assignment 2.

---
+++ Question 6.py
@@ -0,0 +1,34 @@
+# Question 6 - Crime levels and comparing unemployment rates between the groups.
+#==================================================================================
+
+import pandas as pd
+import numpy as np
+
+def convert_csv_as_dataframe(crime_file):
+    crime_dataframe = pd.read_csv(crime_file)
+
+    return crime_dataframe
+
+
+def create_column_risk(crime_dataframe):
+    crime_dataframe["Risk"] = np.where(crime_dataframe["ViolentCrimesPerPop"] >= 0.5, "HighCrime", "LowCrime")
+
+def group_data_by_risk(crime_dataframe_with_risk):
+    grouped_crime_dataframe = crime_dataframe_with_risk.groupby("Risk").agg({
+        "PctUnemployed": "mean",
+    }).reset_index()
+
+    return grouped_crime_dataframe
+
+def print_highcrime_lowcrime_unemployment_averages(risk_crime_dataframe):
+    print(f"Unemployment rate for high crime individuals: {risk_crime_dataframe["PctUnemployed"][0]}")
+    print(f"Unemployment rate for low crime individuals: {risk_crime_dataframe["PctUnemployed"][1]}")
+
+def main():
+    with open("crime.csv", "r") as crime_information_file:
+        crime_information_dataframe = convert_csv_as_dataframe(crime_information_file)
+        create_column_risk(crime_information_dataframe)
+        risk_crime_dataframe = group_data_by_risk(crime_information_dataframe)
+        print_highcrime_lowcrime_unemployment_averages(risk_crime_dataframe)
+
+main()
