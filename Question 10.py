0c756e41b985c8803d027c02a5d6ad9fd411b8bc
adennguyen21 <adennguyen21@gmail.com>
2026-02-11 11:24:02 -0700

    Add comments for Question 10, Assignment 2.

--- Question 10.py
+++ Question 10.py
@@ -2,21 +2,23 @@
 #=================================================================================
 
 def find_lines_containing(filename, keyword):
-
+    # Opens the file, and finds lines containing the given keyword.
     matching_results = []
 
     with open(filename, "r") as file:
         for line_number, line in enumerate(file, start=1):
             if keyword.lower() in line.lower():
+                # Compares the lowercase version of the keyword, to the lowercase version to the line.
                 matching_results.append((line_number, line.strip()))
 
     return matching_results
 
 def print_matching_results(matching_lines):
+    # Prints the results of matching lines to the keyword.
     print(f"Number of matching lines: {len(matching_lines)}")
     print()
 
-    if matching_lines == []:
+    if matching_lines == []: # If there's no matching lines
         print("No matching lines found.")
 
     else:
@@ -24,6 +26,7 @@ def print_matching_results(matching_lines):
         for line_number, line in matching_lines[:3]:
             print(f"Line {line_number}: {line}")
 
+# ============================================================================
 
 def main():
     test_file = "sample-file.txt"
@@ -32,6 +35,5 @@ def main():
     matching_lines = find_lines_containing(test_file, test_keyword)
 
     print_matching_results(matching_lines)
-
 
 main()
