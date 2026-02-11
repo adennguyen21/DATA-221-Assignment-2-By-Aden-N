43b3636f73b48f1d4c7b06ab619d507ad8ead3c0
adennguyen21 <adennguyen21@gmail.com>
2026-02-11 10:55:14 -0700

    Add comments for Question , Assignment 2.

--- Question 6.py
+++ Question 6.py
@@ -5,15 +5,19 @@ import pandas as pd
 import numpy as np
 
 def convert_csv_as_dataframe(crime_file):
+    # Converts the crime csv as a dataframe.
     crime_dataframe = pd.read_csv(crime_file)
 
     return crime_dataframe
 
 
 def create_column_risk(crime_dataframe):
+    # Creates a new column in the dataframe called "risk", with risk being high crime or low crime.
     crime_dataframe["Risk"] = np.where(crime_dataframe["ViolentCrimesPerPop"] >= 0.5, "HighCrime", "LowCrime")
 
+
 def group_data_by_risk(crime_dataframe_with_risk):
+    # Groups the data by the risk column, with the average value for the percent of unemployed for each risk group.
     grouped_crime_dataframe = crime_dataframe_with_risk.groupby("Risk").agg({
         "PctUnemployed": "mean",
     }).reset_index()
@@ -21,14 +25,22 @@ def group_data_by_risk(crime_dataframe_with_risk):
     return grouped_crime_dataframe
 
 def print_highcrime_lowcrime_unemployment_averages(risk_crime_dataframe):
+    # Prints out the unemployment averages for each risk group.
     print(f"Unemployment rate for high crime individuals: {risk_crime_dataframe["PctUnemployed"][0]}")
     print(f"Unemployment rate for low crime individuals: {risk_crime_dataframe["PctUnemployed"][1]}")
 
+# ===================================================================================
+
 def main():
     with open("crime.csv", "r") as crime_information_file:
+
         crime_information_dataframe = convert_csv_as_dataframe(crime_information_file)
+
         create_column_risk(crime_information_dataframe)
+
         risk_crime_dataframe = group_data_by_risk(crime_information_dataframe)
+
         print_highcrime_lowcrime_unemployment_averages(risk_crime_dataframe)
+
 
 main()
