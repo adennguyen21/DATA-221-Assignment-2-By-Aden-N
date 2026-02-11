e74e7d12ec0ac8a94d723b0e92d8fdcadd4818ae
adennguyen21 <adennguyen21@gmail.com>
2026-02-11 11:18:32 -0700

    Add comments for Question 8, Assignment 2.

--- Question 8.py
+++ Question 8.py
@@ -1,7 +1,10 @@
+# Question 8 - Extracting structured heading information from webpage.
+
 from bs4  import BeautifulSoup
 import requests
 
 def load_soup(data_url):
+    # Scraps the wiki and combines all the data into a "soup".
     headers = {"User-Agent": "Mozilla/5.0"}
     wiki_contents = requests.get(data_url, headers=headers)
 
@@ -9,6 +12,7 @@ def load_soup(data_url):
 
 
 def extract_headings(soup):
+    # Finds the headings from the soup, and puts them into a list.
     headings_content = soup.find("div", id="mw-content-text")
 
     headings = []
@@ -22,6 +26,7 @@ def extract_headings(soup):
 
 
 def clean_headings(unclean_headings):
+    # Cleans the headings from excluded words.
     excluded_words = ["References", "External links", "See also", "Notes"]
     final_headings = []
 
@@ -37,16 +42,23 @@ def clean_headings(unclean_headings):
 
 
 def create_headings_file(headings_list):
+    # Creates a file with the headings in the file in order.
     with open("headings.txt", "w") as headings_file:
         for heading in headings_list:
             headings_file.write(heading + "\n")
 
+# =======================================================================================
 
 def main():
     wiki_page = "https://en.wikipedia.org/wiki/Data_science"
+
     data_science_soup = load_soup(wiki_page)
+
     unclean_headings = extract_headings(data_science_soup)
+
     final_headings = clean_headings(unclean_headings)
+
     create_headings_file(final_headings)
+
 
 main()
