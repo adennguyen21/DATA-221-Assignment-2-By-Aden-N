3ac4a87424054655d088875d96af96fe89c0bd24
adennguyen21 <adennguyen21@gmail.com>
2026-02-11 11:06:11 -0700

    Add comments for Question 7, Assignment 2.

--- Question 7.py
+++ Question 7.py
@@ -6,12 +6,14 @@ from bs4 import BeautifulSoup
 
 
 def load_soup(data_url):
+    # Scraps the wiki website and put the data into one big "soup".
     headers = {"User-Agent": "Mozilla/5.0"}
     wiki_contents = requests.get(data_url, headers=headers)
     return BeautifulSoup(wiki_contents.text, "html.parser")
 
 
 def get_page_title(soup):
+    # Finds the page title and prints it out.
     page_title = soup.find("title")
     page_title = page_title.text
 
@@ -19,6 +21,8 @@ def get_page_title(soup):
 
 
 def get_first_paragraph(soup):
+    # Finds the first paragraph from the main content, and prints it out.
+    # (Printing was not needed but I put it in to show.)
     wiki_text_content = soup.find("div", id="mw-content-text")
     all_paragraphs = wiki_text_content.find_all("p")
 
@@ -34,12 +38,17 @@ def get_first_paragraph(soup):
 
     print(f"The first paragraph from the main article is: \n{first_valid_paragraph}")
 
+# ======================================================================================
 
 def main():
     wiki_page = "https://en.wikipedia.org/wiki/Data_science"
+
     data_science_soup = load_soup(wiki_page)
+
     get_page_title(data_science_soup)
+
     print()
+
     get_first_paragraph(data_science_soup)
 
 main()
